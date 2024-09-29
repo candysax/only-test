69562f4d76b97832d560df85af110e4e5a39bbbc
candydice <ishin.daniil@gmail.com>
2024-09-30 02:39:23 +0300

    добавлены хэлперы для работы с путями

--- src/helpers.php
+++ src/helpers.php
@@ -5,7 +5,7 @@ use Only\Test\Base\View;
 function abort(int $code = 404): void
 {
     http_response_code($code);
-    View::render('404');
+    View::render("errors/{$code}");
     die();
 }
 
@@ -13,4 +13,14 @@ function redirect(string $path): void
 {
     header("location: {$path}");
     die();
+}
+
+function base_url(string $path): string
+{
+    return BASE_URL . $path;
+}
+
+function view_url(string $path): string
+{
+    return VIEW_URL . $path;
 }
