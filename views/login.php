eeeef05902d6c21f849afe837d3df4b67041c85d
candydice <ishin.daniil@gmail.com>
2024-09-30 06:09:05 +0300

    изменены теги для отображения ошибок валидации

--- views/login.php
+++ views/login.php
@@ -4,21 +4,21 @@
 <form action="/login" method="POST">
     <input type="hidden" name="_token" value="<?= $_SESSION['_token'] ?? '' ?>">
     <div>
-        <label for="phone_or_email">Телефон или email:</label><br>
+        <label for="phone_or_email">Телефон или email:</label>
         <input type="text" name="phone_or_email" id="phone_or_email">
         <?php if (isset($_SESSION['_flash']['errors']['phone_or_email'])) : ?>
-            <p><?= $_SESSION['_flash']['errors']['phone_or_email'][0] ?></p>
+            <small><?= $_SESSION['_flash']['errors']['phone_or_email'][0] ?></small>
         <?php endif; ?>
     </div>
     <div>
-        <label for="password">Пароль:</label><br>
+        <label for="password">Пароль:</label>
         <input type="password" name="password" id="password">
         <?php if (isset($_SESSION['_flash']['errors']['password'])) : ?>
-            <p><?= $_SESSION['_flash']['errors']['password'][0] ?></p>
+            <small><?= $_SESSION['_flash']['errors']['password'][0] ?></small>
         <?php endif; ?>
     </div>
     <?php if (isset($_SESSION['_flash']['errors']['global'])) : ?>
-        <p><?= $_SESSION['_flash']['errors']['global'][0] ?></p>
+        <small><?= $_SESSION['_flash']['errors']['global'][0] ?></small>
     <?php endif; ?>
     <div>
         <button type="submit">Войти</button>
--- views/register.php
+++ views/register.php
@@ -4,38 +4,38 @@
 <form action="/register" method="POST">
     <input type="hidden" name="_token" value="<?= $_SESSION['_token'] ?? '' ?>">
     <div>
-        <label for="phone">Телефон:</label><br>
+        <label for="phone">Телефон:</label>
         <input type="text" name="phone" id="phone">
         <?php if (isset($_SESSION['_flash']['errors']['phone'])) : ?>
-            <p><?= $_SESSION['_flash']['errors']['phone'][0] ?></p>
+            <small><?= $_SESSION['_flash']['errors']['phone'][0] ?></small>
         <?php endif; ?>
     </div>
     <div>
-        <label for="email">Email:</label><br>
+        <label for="email">Email:</label>
         <input type="email" name="email" id="email">
         <?php if (isset($_SESSION['_flash']['errors']['email'])) : ?>
-            <p><?= $_SESSION['_flash']['errors']['email'][0] ?></p>
+            <small><?= $_SESSION['_flash']['errors']['email'][0] ?></small>
         <?php endif; ?>
     </div>
     <div>
-        <label for="username">Никнейм:</label><br>
+        <label for="username">Никнейм:</label>
         <input type="text" name="username" id="username">
         <?php if (isset($_SESSION['_flash']['errors']['username'])) : ?>
-            <p><?= $_SESSION['_flash']['errors']['username'][0] ?></p>
+            <small><?= $_SESSION['_flash']['errors']['username'][0] ?></small>
         <?php endif; ?>
     </div>
     <div>
-        <label for="password">Пароль:</label><br>
+        <label for="password">Пароль:</label>
         <input type="password" name="password" id="password">
         <?php if (isset($_SESSION['_flash']['errors']['password'])) : ?>
-            <p><?= $_SESSION['_flash']['errors']['password'][0] ?></p>
+            <small><?= $_SESSION['_flash']['errors']['password'][0] ?></small>
         <?php endif; ?>
     </div>
     <div>
-        <label for="password_repeat">Повторите пароль:</label><br>
+        <label for="password_repeat">Повторите пароль:</label>
         <input type="password" name="password_repeat" id="password_repeat">
         <?php if (isset($_SESSION['_flash']['errors']['password_repeat'])) : ?>
-            <p><?= $_SESSION['_flash']['errors']['password_repeat'][0] ?></p>
+            <small><?= $_SESSION['_flash']['errors']['password_repeat'][0] ?></small>
         <?php endif; ?>
     </div>
     <div>
